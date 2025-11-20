b6a9d7f3c6245f36f463232fae2fc40fcedaccf3
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-11-20 12:59:14 +0900

    fix: faviconのUriのgetコード直し忘れ

--- lib/meta/site.dart
+++ lib/meta/site.dart
@@ -11,7 +11,7 @@ class SiteMeta {
   final String name = "地域民botポータル";
   final String name2 = "LocalDB: Portal";
   final String desc = "地域民botに関する情報を纏めた、地域民botを一覧・検索出来る、地域民botのためのデータベース的ポータルサイト";
-  final Uri fav = this.rawDomain.cd(<String>[]);
+  final Uri fav = Meta.assets.favicon;
   final Color = Colors.core;
 
   // Domains & Urls
